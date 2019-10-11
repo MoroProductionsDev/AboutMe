a308d0852fb246afd1f97e2502a8466dfdef6381
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-10-11 00:32:12 -0700

    - added new data class to "java/com.example.aboutme" that   will hold the data for the app.

---
+++ app/src/main/java/com/example/aboutme/MyName.kt
@@ -0,0 +1,7 @@
+package com.example.aboutme
+// this class works as struct.
+// It helps store basic data related to app
+data class MyName(
+    var name : String = "",
+    var nickname :  String = ""
+)
