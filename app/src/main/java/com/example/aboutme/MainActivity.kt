9bb1bb429696bf6ebbaca3d6ac5a690e0451262a
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-10-10 23:37:01 -0700

    - in MainActivity.kt added multiple binding options for performance improve.
    [Binding allow the program to have an intermediate object
    in which will hold a reference to the xml element avoiding
    having to traverse multiple times to xml hierarchy]

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,29 +8,44 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding //  binding object (wil bind layout with activity main)
     lateinit var editText : EditText
     lateinit var nicknameTextview: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.id_btn_done).setOnClickListener {
+        //findViewById<Button>(R.id.id_btn_done).setOnClickListener {
+        //    addNickName(it)  // it is the btn_done
+       //}
+        binding.idBtnDone.setOnClickListener{
             addNickName(it)  // it is the btn_done
-       }
+        }
 
+        /*
         editText = findViewById(R.id.id_textfield_nickname)
         nicknameTextview = findViewById(R.id.id_textview_nickname)
+        */
     }
 
     private fun addNickName(view : View) {
-        nicknameTextview.text = editText.text  // set nickname texfield.text nickname to textview.text
-        editText.visibility = View.GONE // nickname texfield
-        view.visibility = View.GONE  // done button
-        nicknameTextview.visibility = View.VISIBLE  // nickname textview
+        // nicknameTextview.text = editText.text  // set nickname texfield.text nickname to textview.text
+        // editText.visibility = View.GONE // nickname texfield
+        // view.visibility = View.GONE  // done button
+        // nicknameTextview.visibility = View.VISIBLE  // nickname textview
+        binding.idTextviewNickname.text = editText.text
+        binding.idTextfieldNickname.visibility = View.GONE
+        binding.idBtnDone.visibility = View.GONE
+        binding.idTextviewNickname.visibility = View.VISIBLE
+
 
         // hide the keyboard
         val board = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
